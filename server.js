122e933e1eea6f2a455a9a4880ea9ca88a57c144
serena-ramley <sramley@gmail.com>
2021-02-06 19:05:00 -0800

    Updated post request

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,24 +9,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Our App' });
+app.get("/api/hello", (req, res) => {
+  res.send({ express: "Hello From Our App" });
 });
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
+app.post("/api/world", (req, res) => {
+  console.log(req.body.post);
+  console.log(req.body.key);
+  console.log(req.body.language);
+
+  // Add key, post (i.e. translation) and language to database
+
   res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
+    `I received your POST request. This is what you sent me: ${req.body.post}`
   );
 });
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
